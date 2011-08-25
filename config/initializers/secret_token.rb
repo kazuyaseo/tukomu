b95d13c6f78a86749d4f8e2a04a2a344213f863f
瀬尾 一矢 <seokazuya@seomac.local>
2011-08-25 11:58:29 +0900

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tukomu::Application.config.secret_token = 'd8c092df3f1f924fb03e267796fb2d1b8cea48dac88cd1b039bc16cac7430a55141c711c29adbf1a8ed9c7de0c72352b50a9801eef96bc6f3a5517523d49a7a0'
